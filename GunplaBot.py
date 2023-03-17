cc0da4a5f015ff22c9dd51c0a3726ee42bac4800
mori-C <kilbara.h.morikawa@gmail.com>
2023-03-17 10:36:51 +0900

    delete json module

--- GunplaBot.py
+++ GunplaBot.py
@@ -12,8 +12,8 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 import setup
 
-j = open('secret.json','r')
-secret = json.load(j)
+#j = open('secret.json','r')
+#secret = json.load(j)
 
 DISCORD_TOKEN = setup.TOKEN
 CHROMEDRIVER = "/usr/bin/chromedriver"
